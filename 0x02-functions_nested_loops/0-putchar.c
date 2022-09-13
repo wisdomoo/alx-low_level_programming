2a4dec43436c13dec50e54cbca6e784ef27652ed
wisdomoo <okoroaforwisdomo@gmail.com>
2022-09-13 07:11:46 -0700

    files added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char _putchar[8] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+		putchar(_putchar[i]);
+		
+
+	return 0;
+}
+
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char _putchar(char a){
+	putchar(a)
+}
+
