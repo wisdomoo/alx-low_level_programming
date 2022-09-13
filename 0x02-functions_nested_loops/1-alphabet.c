83b51ae17db466a24e6421499493437b9eaa3bdc
wisdomoo <okoroaforwisdomo@gmail.com>
2022-09-13 08:32:18 -0700

    files added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    void print_alphabet(void);
+    int lower;
+    for (lower = 'a'; lower <= 'z'; lower++)
+    {
+	    _putchar(lower);
+    }
+    _putchar('\n');
+
+    return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 
 int _putchar(char a);
-
+void print_alphabet(void);
 #endif
