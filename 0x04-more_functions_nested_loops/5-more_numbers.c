d479c9239c7cc5ae750ac8ec7b636f86378677a2
wisdomoo <okoroaforwisdomo@gmail.com>
2022-09-15 05:56:26 -0700

    files added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 15; j++)
+	{
+		for (i = 0; i < 10; i++)
+			_putchar(i + '0');
+		_putchar('\n');
+	}
+}
