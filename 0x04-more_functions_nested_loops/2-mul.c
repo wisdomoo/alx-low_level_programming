6d3a4c488d37f3168c9a048b4f6c61cad5ca2133
wisdomoo <okoroaforwisdomo@gmail.com>
2022-09-15 04:49:08 -0700

    files added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers together
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
