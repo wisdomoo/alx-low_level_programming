1810ab080fa0f803bbede5d04400b850687b23cb
wisdomoo <okoroaforwisdomo@gmail.com>
2022-09-15 05:25:12 -0700

    files added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+		_putchar('\n');
+}
