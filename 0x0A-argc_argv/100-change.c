b83516e70dec87cf2446bf49c0adffdbef81a85e
wisdomoo <okoroaforwisdomo@gmail.com>
2022-10-01 11:27:02 -0700

    files added

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int change(int cents);
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else if (argc < 0)
+	{
+		return (0);
+	}
+
+	printf("%d\n", change(atoi(argv[1])));
+	return (0);
+}
+
+/**
+ * change - get change
+ * @cents: amount of coins from main function
+
