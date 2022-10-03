d8bf84e05ee264b3a9afbac38c6f906f421b689c
wisdomoo <okoroaforwisdomo@gmail.com>
2022-10-03 04:18:57 -0700

    correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * create_array - main entry
- * @size: size input
- * @c: char
- * Return: 0
+ * _strdup - A function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as a
+ * parameter.
+ * @str: An input pointer of the string to copy.
+ * Return: Apointer to new string or NULL if it str is NULL
  */
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
-	char *array;
-	unsigned int i;
+	char *new_str, *start;
+	int i = 0, len = 0;
 
-	array = malloc(sizeof(char) * size);
-
-	if (size == 0 || array == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		array[i] = c;
-	return (array);
+	start = str;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+
+	str = start;
+	new_str = malloc(sizeof(char) * (len + 1));
+	start = new_str;
+
+	if (new_str != NULL)
+	{
+		for (; i < len; i++)
+		{
+			new_str[i] = *str;
+			str++;
+		}
+		new_str[i] = '\0';
+		return (start);
+	}
+	else
+		return (NULL);
 }
